ad5efbae192e41c0045ce3ea6c43b21b64a71724
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-28 15:08:37 +0700

    Add comments to article model for future development

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :slug, presence: true, uniqueness: true
+  # for the slug should add random number
+  # slug = title + random_number(maybe add DateTime published?)
 end

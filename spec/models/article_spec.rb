b7c82f26414381eb8b39e0a6955ad98dd33667a9
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-28 15:35:36 +0700

    Update double quote to single quote

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe "#validations" do
-    it "should test that the Factory Bot is valid" do
+  describe '#validations' do
+    it 'should test that the Factory Bot is valid' do
       expect(build :article).to be_valid
     end
 
-    it "should validate the presence of the Title" do
-      article = build :article, title: ""
+    it 'should validate the presence of the Title' do
+      article = build :article, title: ''
 
       expect(article).not_to be_valid
       expect(article.errors.messages[:title]).to include("can't be blank")
     end
 
-    it "should validate the presence of the Content" do
-      article = build :article, content: ""
+    it 'should validate the presence of the Content' do
+      article = build :article, content: ''
 
       expect(article).not_to be_valid
       expect(article.errors.messages[:content]).to include("can't be blank")
     end
 
-    it "should validate the presence of the Slug" do
+    it 'should validate the presence of the Slug' do
       # Slug is used by frontend to tell which URL will be use by the client
       # to identifies the Article, slug should be always present and uniq
-      article = build :article, slug: ""
+      article = build :article, slug: ''
 
       expect(article).not_to be_valid
       expect(article.errors.messages[:slug]).to include("can't be blank")
     end
 
-    it "should validate the uniqueness of the Slug" do
+    it 'should validate the uniqueness of the Slug' do
       article = create :article
 
       invalid_article = build :article, slug: article.slug

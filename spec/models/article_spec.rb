6aca7c67ed264383343baf2c4f6392065e0a95c5
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-28 15:24:30 +0700

    Refactoring setup: - add FactoryBot syntax method to rails_helper - remove FactoryBot. from article test - fix slug test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,37 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "should test that the Factory Bot is valid" do
-    expect(FactoryBot.build :article).to be_valid
-  end
+  describe "#validations" do
+    it "should test that the Factory Bot is valid" do
+      expect(build :article).to be_valid
+    end
 
-  it "should validate the presence of the Title" do
-    article = FactoryBot.build :article, title: ""
+    it "should validate the presence of the Title" do
+      article = build :article, title: ""
 
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:title]).to include("can't be blank")
-  end
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
 
-  it "should validate the presence of the Content" do
-    article = FactoryBot.build :article, content: ""
+    it "should validate the presence of the Content" do
+      article = build :article, content: ""
 
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:content]).to include("can't be blank")
-  end
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
 
-  it "should validate the presence of the Slug" do
-    # Slug is used by frontend to tell which URL will be use by the client
-    # to identifies the Article, slug should be always present and uniq
-    article = FactoryBot.build :article, slug: ""
+    it "should validate the presence of the Slug" do
+      # Slug is used by frontend to tell which URL will be use by the client
+      # to identifies the Article, slug should be always present and uniq
+      article = build :article, slug: ""
 
-    expect(article).not_to be_valid
-    expect(article.errors.messages[:slug]).to include("can't be blank")
-  end
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
 
-  it "should validate the presence of the Slug" do
-    article = FactoryBot.build :article
+    it "should validate the uniqueness of the Slug" do
+      article = create :article
 
-    invalid_article = FactoryBot.build :article, slug: article.slug
-    expect(invalid_article).not_to be_valid
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end
   end
 end

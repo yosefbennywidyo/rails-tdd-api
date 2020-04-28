4174ef778c598b57835a3362621c3524a6dd8f34
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-28 14:54:29 +0700

    Create article test: - to check the presence of the Title and Content

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should test that the Factory Bot is valid" do
+    expect(FactoryBot.build :article).to be_valid
+  end
+
+  it "should validate the presence of the Title" do
+    article = FactoryBot.build :article, title: ""
+
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:title]).to include("can't be blank")
+  end
+
+  it "should validate the presence of the Content" do
+    article = FactoryBot.build :article, content: ""
+
+    expect(article).not_to be_valid
+    expect(article.errors.messages[:content]).to include("can't be blank")
+  end
 end

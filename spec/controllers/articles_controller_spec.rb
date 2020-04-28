02f2f51fc7a2497400b3aeff79e7692ca58efd5e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-29 00:00:54 +0700

    Refactoring articles controller spec: - create JsonApiHelpers - enable support directory and include JsonApiHelpers - refactoring articles_controller_spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,27 +2,23 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe 'index' do
+    subject { get :index }
+
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
     it 'should return proper JSON' do
-      create_list :article, 2
-      get :index
-      json = JSON.parse(response.body)
+      articles = create_list :article, 2
+      subject
       #pp json # pp use to print json in console
-      json_data = json['data']
-      expect(json_data.length).to eq(2)
-      expect(json_data[0]['attributes']).to eq({
-        "title" => "My article title 1",
-        "content" => "My content article 1",
-        "slug" => "my-article-title-1"
-      })
-      expect(json_data[1]['attributes']).to eq({
-        "title" => "My article title 2",
-        "content" => "My content article 2",
-        "slug" => "my-article-title-2"
-      })
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+          "title" => article.title,
+          "content" => article.content,
+          "slug" => article.slug
+        })
+      end
     end
   end
 end

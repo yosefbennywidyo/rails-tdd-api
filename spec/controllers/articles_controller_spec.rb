caf1012c5a1224d9263514f5c2734282863f6efe
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-29 10:30:41 +0700

    Add test for article: - return articles in the proper order

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -8,6 +8,7 @@ describe ArticlesController do
       subject
       expect(response).to have_http_status(:ok)
     end
+
     it 'should return proper JSON' do
       articles = create_list :article, 2
       subject
@@ -19,6 +20,14 @@ describe ArticlesController do
           "slug" => article.slug
         })
       end
+    end
+
+    it 'should return articles in the proper order' do
+      old_article = create :article
+      newer_article = create :article
+      subject
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
     end
   end
 end

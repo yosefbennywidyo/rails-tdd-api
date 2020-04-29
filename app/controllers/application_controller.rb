c05b0e9addfcc35678e1ef63e910d2dfcc670056
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-29 15:48:35 +0700

    Update application controller: - add jsonapi_errors_handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  require 'jsonapi_errors_handler'
+
+  JsonapiErrorsHandler.configure do |config|
+    config.handle_unexpected = true
+  end
 end

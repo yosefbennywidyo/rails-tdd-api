d605547bfad0567738530bc272a74698c6e6f233
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-04-29 10:31:38 +0700

    Update article models: - add scope :recent to oder by newer

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
   validates :slug, presence: true, uniqueness: true
   # for the slug should add random number
   # slug = title + random_number(maybe add DateTime published?)
+  scope :recent, -> { order(created_at: :desc) }
 end

fa2b0bb2d3940949b0404b97d8471a465d197c0a
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-01 17:14:15 +0700

    Setup for users with FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    login { "MyString" }
-    name { "MyString" }
-    url { "MyString" }
-    avatar_url { "MyString" }
-    provider { "MyString" }
+    sequence(:login) { |n| "fybwid#{n}" }
+    name "Yosef Benny"
+    url "http://example.com"
+    avatar_url "http://example.com/avatar"
+    provider "github"
   end
 end

e53f97f7cf24920fa188a751ce3b3c96643822ad
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-01 17:34:00 +0700

    Fix users factories format

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     sequence(:login) { |n| "fybwid#{n}" }
-    name "Yosef Benny"
-    url "http://example.com"
-    avatar_url "http://example.com/avatar"
-    provider "github"
+    name { "Yosef Benny" }
+    url { "http://example.com" }
+    avatar_url { "http://example.com/avatar" }
+    provider { "github" }
   end
 end

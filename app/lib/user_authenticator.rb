624a1efd8c065235d5a7b6e3a6d2dc2f4ed79033
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 03:01:43 +0700

    Update user authenticator service: - update perform to check if user already registered

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -20,7 +20,11 @@ class UserAuthenticator
         access_token: token)
       user_data = user_client.user.to_h.
         slice(:login, :avatar_url, :url, :name)
-      User.create(user_data.merge(provider: 'github'))
+      @user = if User.exists?(login: user_data[:login])
+        User.find_by(login: user_data[:login])
+      else
+        User.create(user_data.merge(provider: 'github'))
+      end
     end
   end
 

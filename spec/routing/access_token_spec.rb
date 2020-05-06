ed6c9563d4d513830c0c75ff3487092075076fa7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 14:13:58 +0700

    Create access token routing test

---
+++ spec/routing/access_token_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'access token routes' do
+  it 'should route to access_token create action' do
+    expect(post '/login').to route_to('access_token#create')
+  end
+end

7d770451a59edab23550428483cead7474a30326
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 14:12:25 +0700

    Create access token model test: - validations - new

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe AccessToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should have valid factory' do
+      #access_token = build :access_token
+      #expect(access_token).to be_valid
+    end
+
+    it 'should validate token' do
+      
+    end
+  end
+
+  describe '#new' do
+    it 'should have a token present after initialize' do
+      expect(AccessToken.new.token).to be_present
+    end
+
+    it 'should generate uniq token' do
+      user = create :user
+      expect{ user.create_access_token }.to change{ AccessToken.count }.by(1)
+      expect(user.build_access_token).to be_valid
+    end
+  end
 end

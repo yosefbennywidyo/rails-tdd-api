4e5421e5fe10b28444ece296674c37e9ed198a9e
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 15:17:28 +0700

    Fix access token routing test: - update to access_tokens

--- spec/routing/access_token_spec.rb
+++ spec/routing/access_token_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe 'access token routes' do
   it 'should route to access_token create action' do
-    expect(post '/login').to route_to('access_token#create')
+    expect(post '/login').to route_to('access_tokens#create')
   end
 end

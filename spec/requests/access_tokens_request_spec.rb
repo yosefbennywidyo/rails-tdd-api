1c399897c12fcd76b6cde43abbd843e25e0e549a
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 14:19:19 +0700

    Create access tokens request test: - create

---
+++ spec/requests/access_tokens_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "AccessTokens", type: :request do
+  describe '#create' do
+    context 'when invalid request' do
+      let(:error) do
+        {
+          "status": "401",
+          "source": { "pointer": "/code" },
+          "title":  "Authentication code is invalid",
+          "detail": "Yout must provide valid code in order to exchange it for token"
+        }
+      end
+      it 'should return 401 status code' do
+        post :create
+        expect(response).to have_http_status(401)
+      end
+
+      it 'should return proper error body' do
+        post :create
+        expect(json['errors']).to include(error)
+      end
+    end
+
+    context 'when invalid request' do
+      
+    end
+  end
+end

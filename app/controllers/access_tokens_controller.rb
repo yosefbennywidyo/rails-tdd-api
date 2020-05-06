4d68b549cfc54d8ff88d9d0b0d543033785a047f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 15:10:46 +0700

    Update application and access tokens controller: - move authentication error to application controller   so it can be used accross controller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,20 +1,8 @@
 class AccessTokensController < ApplicationController
-  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
 
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
   end
 
-  private
-
-  def authentication_error
-    error = {
-      "status" => "401",
-      "source" => { "pointer" => "/code" },
-      "title" =>  "Authentication code is invalid",
-      "detail" => "Yout must provide valid code in order to exchange it for token"
-    }
-    render json: { "errors": [ error ] }, status: 401
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::API
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+=begin
   require 'jsonapi_errors_handler'
 
   JsonapiErrorsHandler.configure do |config|
     config.handle_unexpected = true
+  end
+=end
+  private
+
+  def authentication_error
+    error = {
+      "status" => "401",
+      "source" => { "pointer" => "/code" },
+      "title" =>  "Authentication code is invalid",
+      "detail" => "Yout must provide valid code in order to exchange it for token"
+    }
+    render json: { "errors": [ error ] }, status: 401
   end
 end

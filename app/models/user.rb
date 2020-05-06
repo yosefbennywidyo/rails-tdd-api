4c344b2ea313111d1af54313744d95de2cedf281
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-06 14:15:44 +0700

    Update user model: - add validates for access_token - relations with access_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
+  validates :access_token, presence: true, uniqueness: true
+
+  has_one :access_token, dependent: :destroy
 end

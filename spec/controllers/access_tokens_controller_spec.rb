7595f51e136ef13f7a78b30f0f3b965746ee9f51
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-07 15:18:43 +0700

    Update access tokens test: - move code from request to controller - refactoring for unauthorized request and   forbidden request to use accros controllers

---
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe AccessTokensController, type: :controller do
+  describe 'POST #create' do
+    context 'when no code provided' do
+      subject { post :create }
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context 'when invalid code provided' do
+      let(:github_error) {
+        double("Sawyer::Resource", error: "bad_verification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(github_error)
+      end
+
+      subject { post :create, params: { code: 'invalid_code' } }
+
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context 'when success request' do
+      let(:user_data) do
+        {
+          login: 'fybwid',
+          url: 'http://example.com',
+          avatar_url: 'http://example.com/avatar',
+          name: 'Yosef Benny Widyokarsono'
+        }
+      end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return('validaccesstoken')
+
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user).and_return(user_data)
+      end
+
+      subject { post :create, params: { code: 'valid_code' } }
+
+      it 'should return 201 status code' do
+        subject
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'should return proper json body' do
+        expect{ subject }.to change{ User.count }.by(1)
+        user = User.find_by(login: 'fybwid')
+        #This use to print the result on command line
+        #pp json_data['attributes']
+        #pp user.access_token.token
+        expect(json_data['attributes']).to eq(
+          { 'token' => user.access_token.token }
+        )
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject { delete :destroy }
+
+    context 'when no authorization header provided' do
+
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when invalid authorization header provided' do
+      before { request.headers['authorization'] = 'Invalid token' }
+
+      it_behaves_like 'forbidden_requests'
+    end
+
+    context 'when valid request' do
+
+    end
+  end
+end

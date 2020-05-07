ed147fcdbd4a1c5f72677be5f062ca312b79b3bc
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-07 15:39:35 +0700

    Update application controllers: - add AuthorizationError class  - add authorization_error method  - fix detail text on authentication_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
+  class AuthorizationError < StandardError; end
+
   rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+  rescue_from AuthorizationError, with: :authorization_error
 =begin
   require 'jsonapi_errors_handler'
 
@@ -14,8 +17,18 @@ class ApplicationController < ActionController::API
       "status" => "401",
       "source" => { "pointer" => "/code" },
       "title" =>  "Authentication code is invalid",
-      "detail" => "Yout must provide valid code in order to exchange it for token"
+      "detail" => "You must provide valid code in order to exchange it for token."
     }
     render json: { "errors": [ error ] }, status: 401
+  end
+
+  def authorization_error
+    error = {
+      "status" => "403",
+      "source" => { "pointer" => "/headers/authorization" },
+      "title" =>  "Not authorized",
+      "detail" => "You have no right to access this resource."
+    }
+    render json: { "errors": [ error ] }, status: 403
   end
 end

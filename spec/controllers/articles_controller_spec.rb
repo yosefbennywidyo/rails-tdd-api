e1bd30de1e295a8e58d6cf129d9e02582d8f3c0f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-08 13:51:51 +0700

    Update articles controller test: - add when success request sent test - fix invalid_attributes to string

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -77,10 +77,10 @@ describe ArticlesController do
       context 'when invalid parameters provided' do
         let(:invalid_attributes) do
           {
-            data: {
-              attributes: {
-                title: '',
-                content: ''
+            'data': {
+              'attributes': {
+                'title': '',
+                'content': ''
               }
             }
           }
@@ -109,6 +109,38 @@ describe ArticlesController do
               "detail" => "can't be blank"
             }
           )
+        end
+      end
+
+      context 'when success request sent' do
+        let(:access_token) { create :access_token }
+        before { request.headers['authorization'] = "Bearer #{access_token.token}" }
+
+        let(:valid_attributes) do
+          {
+            'data': {
+              'attributes': {
+                'title': 'Awesome article',
+                'content': 'Super content',
+                'slug': 'awesome-article'
+              }
+            }
+          }
+        end
+        subject { post :create, params: valid_attributes }
+
+        it 'should have 201 status code' do
+          subject
+          expect(response).to have_http_status(:created)
+        end
+
+        it 'should have proper json body' do
+          subject
+          expect(json_data['attributes']).to include(valid_attributes['data']['attributes'])
+        end
+
+        it 'should create the article' do
+          expect{ subject }.to change{ Article.count }.by(1)
         end
       end
     end

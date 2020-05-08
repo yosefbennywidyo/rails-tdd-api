e5e3d87c8448f9e474f8b941a0cec4c97f86d90b
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-08 13:28:07 +0700

    Update articles test: - articles controller:   - add create test - articles route:   - add route to articles create

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -10,4 +10,8 @@ describe 'article routes' do
   it 'should route to articles show' do
     expect(get '/articles/1').to route_to('articles#show', id: '1')
   end
+
+  it 'should route to articles create' do
+    expect(post '/articles').to route_to('articles#create')
+  end
 end

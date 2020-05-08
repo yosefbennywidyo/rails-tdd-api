470f898f48831b78410ffa7a2eacfefb7d852412
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-08 16:12:25 +0700

    Add user to article migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_055741) do
+ActiveRecord::Schema.define(version: 2020_05_08_081552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_05_06_055741) do
     t.string "slug"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_05_06_055741) do
   end
 
   add_foreign_key "access_tokens", "users"
+  add_foreign_key "articles", "users"
 end

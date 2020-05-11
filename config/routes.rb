46c032a6c0617a69301091cd8f9346d48dacad04
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-11 17:37:06 +0700

    Update routes config: - add sign_up - nested comments on articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
-  resources :articles
+  post 'sign_up', to: 'registrations#create'
+
+  resources :articles do
+    resources :comments, only: [:index, :create]
+  end
 end

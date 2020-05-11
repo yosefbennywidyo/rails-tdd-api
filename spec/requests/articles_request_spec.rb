046695a14afa837b690bf787e290fe50405b13f9
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-11 17:42:08 +0700

    Update requests test: - articles - comments

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -3,7 +3,18 @@ require 'rails_helper'
 # Read more details below:
 # https://relishapp.com/rspec/rspec-rails/docs/request-specs/request-spec
 RSpec.describe "Articles", type: :request do
-  it 'should route to articles index' do
-    expect(get '/articles').to route_to('articles#index')
+  it 'should GET the articles index' do
+    headers = { "ACCEPT" => "application/json" }
+    get "/articles"
+    expect(response.content_type).to eq("application/json; charset=utf-8")
+    expect(response).to have_http_status(:ok)
   end
+=begin
+  it 'should POST an article' do
+    headers = { "ACCEPT" => "application/json" }
+    post "/widgets", :params => { :article => { build :article } }, :headers => headers
+    expect(response.content_type).to eq("application/json; charset=utf-8")
+    expect(response).to have_http_status(:created)
+  end
+=end
 end

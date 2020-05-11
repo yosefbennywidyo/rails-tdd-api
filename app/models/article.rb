3a68d302601d6362259d43cd455d5c49fb501409
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-05-11 17:42:56 +0700

    Update models: - article - comment - user

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,9 @@ class Article < ApplicationRecord
   validates :slug, presence: true, uniqueness: true
 
   belongs_to :user
+
+  has_many :comments, dependent: :destroy
+
   # for the slug should add random number
   # slug = title + random_number(maybe add DateTime published?)
   scope :recent, -> { order(created_at: :desc) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
+  include BCrypt
   validates :login, presence: true, uniqueness: true
   validates :provider, presence: true
-  #validates :access_token, presence: true, uniqueness: true
-
-  has_many :article, dependent: :destroy
+  validates :password, presence: true, if: -> { provider == 'standard' }
 
   has_one :access_token, dependent: :destroy
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def password
+    @password ||= Password.new(encrypted_password) if encrypted_password.present?
+  end
+
+  def password=(new_password)
+    return @password = new_password if new_password.blank?
+    @password = Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
 end
